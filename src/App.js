52b7ad182c6565aac38fe7d2144e8d00e7e263cd
Nguyen Minh Huy <huynmse161323@fpt.edu.vn>
2022-06-26 22:12:43 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+// // import React, {Component} from "react";
+// // import Main from "./components/MainComponent.js"
+// // import "./App.css"
+
+// // class App extends Component {
+// //   render() {
+// //     return (
+// //       <div className="App">
+// //         <Main />
+// //       </div>
+// //     );
+// //   }
+  
+// // }
+// // export default App;
+
 // import React, {Component} from "react";
 // import Main from "./components/MainComponent.js"
 // import "./App.css"
+// import { BrowserRouter } from 'react-router-dom';
 
 // class App extends Component {
 //   render() {
@@ -14,18 +31,41 @@
 // }
 // export default App;
 
-import React, {Component} from "react";
-import Main from "./components/MainComponent.js"
-import "./App.css"
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Main />
-      </div>
-    );
-  }
-  
+import { DISHES } from "./shared/dishes";
+import Main from "./components/MainComponent";
+import Home from "./components/HeaderComponent";
+import { BrowserRouter } from "react-router-dom";
+// function App() {
+//    return (
+//       <div className="App">
+//          <Navbar dark color="primary">
+//             <div className="container">
+//                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//             </div>
+//          </Navbar>
+//       </div>
+//    );
+// }
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         dishes: DISHES,
+      };
+   }
+   render() {
+      return (
+         <BrowserRouter>
+            <div className="App">
+               <Main />
+            </div>
+         </BrowserRouter>
+      );
+   }
 }
+
 export default App;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,38 +1,113 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
-import { DISHES } from '../shared/dishes';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
+// import React, { Component } from 'react';
+// import { Navbar, NavbarBrand } from 'reactstrap';
+// import Menu from './MenuComponent';
+// import DishDetail from './DishDetailComponent';
+// import { DISHES } from '../shared/dishes';
+// import Header from './HeaderComponent';
+// import Footer from './FooterComponent';
+// import Home from './HeaderComponent';
+// import { Switch, Route, Redirect } from 'react-router-dom';
 
 
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        <Footer />
-      </div>
-    );
-  }
+// class Main extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//         dishes: DISHES,
+//       //  selectedDish: null
+//     };
+//   }
+//   // onDishSelect(dishId) {
+//   //   this.setState({ selectedDish: dishId});
+//   // }
+//   render() {
+//     const HomePage = () => {
+//     return (
+//       <Home />
+//     );
+//     };
+//      return(
+
+//       <div>
+//         {
+//         // <Navbar dark color="primary">
+//         //   <div className="container">
+//         //     <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//         //   </div>
+//         // </Navbar> }
+//         <Header /> 
+//         {
+//         // <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+//         // <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+//         }
+//         <Switch>
+//           <Route path ='/home' component={HomePage} />
+//           <Route exact path ='/menu' component={() => <Menu dishes = {this.state.dishes} /> } />
+//           <Redirect to = "/home" />
+//         </Switch>
+
+//       <Footer />
+//       </div>
+//      );
+//       }
+//       }
+
+//       export default Main;
+
+import React from "react";
+import Menu from "./MenuComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { DISHES } from "../shared/dishes";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+class Main extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         selectedDish: null,
+         dishes: DISHES,
+      };
+   }
+   onDishSelect(dish) {
+      this.setState({ selectedDish: dish });
+   }
+   render() {
+      const HomePage = () => {
+         return <Home />;
+      };
+      return (
+         <div>
+            {/* <Navbar dark color="primary">
+               <div className="container">
+                  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+               </div>
+            </Navbar> */}
+            <Header />
+            {/* <Menu
+               dishes={this.state.dishes}
+               click={(dish) => this.onDishSelect(dish)}
+            />
+            <DishDetail dish={this.state.selectedDish} /> */}
+            <Switch>
+               <Route path="/home" component={HomePage} />
+               <Route
+                  exact
+                  path="/menu"
+                  component={() => <Menu dishes={this.state.dishes} />}
+               />
+               <Redirect to="/home" />
+            </Switch>
+            <Footer />
+         </div>
+      );
+   }
 }
+
 export default Main;
+
+
+       
+      
