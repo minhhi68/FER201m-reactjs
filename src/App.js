f6dd6e256e0379dd0d2df4f3750758457b456601
Nguyen Minh Huy <huynmse161323@fpt.edu.vn>
2022-07-05 17:28:40 +0700

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -1,71 +1,25 @@
-// // import React, {Component} from "react";
-// // import Main from "./components/MainComponent.js"
-// // import "./App.css"
 
-// // class App extends Component {
-// //   render() {
-// //     return (
-// //       <div className="App">
-// //         <Main />
-// //       </div>
-// //     );
-// //   }
-  
-// // }
-// // export default App;
-
-// import React, {Component} from "react";
-// import Main from "./components/MainComponent.js"
-// import "./App.css"
-// import { BrowserRouter } from 'react-router-dom';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Main />
-//       </div>
-//     );
-//   }
-  
-// }
-// export default App;
-
-// import logo from "./logo.svg";
+import React, { Component } from "react";
+import Main from "./components/MainComponent.js";
 import "./App.css";
-import React from "react";
-
-import { DISHES } from "./shared/dishes";
-import Main from "./components/MainComponent";
-// import Home from "./components/HomeComponent";
 import { BrowserRouter } from "react-router-dom";
-// function App() {
-//    return (
-//       <div className="App">
-//          <Navbar dark color="primary">
-//             <div className="container">
-//                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//             </div>
-//          </Navbar>
-//       </div>
-//    );
-// }
-class App extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         dishes: DISHES,
-      };
-   }
-   render() {
-      return (
-         <BrowserRouter>
-            <div className="App">
-               <Main />
-            </div>
-         </BrowserRouter>
-      );
-   }
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+
+const store = ConfigureStore();
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
