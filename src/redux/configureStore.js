7a7a2db498720e5d228b86c7aefc5a455585c53f
Nguyen Minh Huy <huynmse161323@fpt.edu.vn>
2022-07-10 18:34:29 +0700

    Intro. to Redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
-import { Reducer, initialState} from './reducer'
+import { Reducer, initialState } from './reducer'
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -8,4 +8,4 @@ export const ConfigureStore = () => {
     );
     
     return store;
-}
+};
--- src/storage/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const LOADDATA = 'LOADDATA';
-export const DISPLAYDATA = 'DISPLAYDATA';
-
